d86f8f9e05a5f4457a69f42181eaa2fc571be277
HeJin <deciphers@163.com>
2023-03-27 13:28:27 +0800

    feat(IO流): 捕获IO异常
    捕获IO异常

--- io-stream/src/main/java/com/sanfen/io/practice/FileCopy2.java
+++ io-stream/src/main/java/com/sanfen/io/practice/FileCopy2.java
@@ -1,5 +1,7 @@
 package com.sanfen.io.practice;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,24 +12,27 @@ import java.io.IOException;
  * @version 1.0
  * @since 2023/03/27 10:54
  */
+@Slf4j
 public class FileCopy2 {
 
     public static void main(String[] args) throws IOException {
-        long start = System.currentTimeMillis();
-        // 1.创建对象
-        FileInputStream fis = new FileInputStream("io-stream\\a.txt");
-        FileOutputStream fos = new FileOutputStream("io-stream\\b.txt");
-        // 2.拷贝
-        int len;
-        byte[] bytes = new byte[1024];
-        while ((len = fis.read(bytes)) != -1){
-            fos.write(bytes, 0, len);
+        String srcFile = "io-stream\\c.txt";
+        String destFile = "io-stream\\b.txt";
+        try (FileInputStream fis = new FileInputStream(srcFile);
+             FileOutputStream fos = new FileOutputStream(destFile)) {
+            long start = System.currentTimeMillis();
+            // 2.拷贝
+            int len;
+            byte[] bytes = new byte[1024];
+            while ((len = fis.read(bytes)) != -1){
+                fos.write(bytes, 0, len);
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("拷贝文件耗时: " + (end - start));
+        } catch (IOException e) {
+            log.error("拷贝文件出错! src: {} =========> dest: {}", srcFile, destFile);
+            throw new RuntimeException(e);
         }
-        // 3.关闭资源
-        fis.close();
-        fos.close();
-        long end = System.currentTimeMillis();
-        System.out.println("拷贝文件耗时: " + (end - start));
     }
 
 }
