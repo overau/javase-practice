1d06e8107280ee1e94525e11996c594e114000bd
HeJin <deciphers@163.com>
2023-04-03 10:40:07 +0800

    feat(IO流): 网络爬虫 - 爬取姓氏
    网络爬虫 - 爬取姓氏

---
+++ io-stream/src/main/java/com/sanfen/smallproject/WebCrawler.java
@@ -0,0 +1,83 @@
+package com.sanfen.smallproject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * 爬取网络上的姓氏和名字
+ * @author HeJin
+ * @version 1.0
+ * @since 2023/04/03 9:52
+ */
+public class WebCrawler {
+
+    public static void main(String[] args) throws IOException {
+        /*
+            百家姓: https://hanyu.baidu.com/s?wd=%E7%99%BE%E5%AE%B6%E5%A7%93&from=poem
+            男性名字: http://www.haoming8.cn/baobao/10881.html
+            女性名字: http://www.haoming8.cn/baobao/7641.html
+         */
+        String familyName = "https://hanyu.baidu.com/s?wd=%E7%99%BE%E5%AE%B6%E5%A7%93&from=poem";
+        String boyNameNet = "http://www.haoming8.cn/baobao/10881.html";
+        String girlNameNet = "http://www.haoming8.cn/baobao/7641.html";
+
+        String familyNameStr = webCrawler(familyName);
+        String boyNameStr = webCrawler(boyNameNet);
+        String girlNameStr = webCrawler(girlNameNet);
+
+        // 正则表达式获取数据
+        List<String> familyNameTempList = getData(familyNameStr, "(.{4})(，|。)", 1);
+        List<String> familyNames = familyNameTempList.stream()
+                .flatMap(f -> Arrays.stream(f.split("")))
+                .collect(Collectors.toList());
+        System.out.println(familyNames);
+    }
+
+    /**
+     * 根据正则表达式获取字符串中的数据
+     * @param contentStr 完整的字符串
+     * @param regex 正则表达式
+     * @param index 正则匹配分组
+     * @return 想要的数据
+     */
+    private static List<String> getData(String contentStr, String regex, int index) {
+        List<String> list = new ArrayList<>();
+        // 正则
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(contentStr);
+        while (matcher.find()){
+            list.add(matcher.group(index));
+        }
+        return list;
+    }
+
+    /**
+     * 网络上爬取数据，拼接成字符串返回
+     * @param net 数据URL
+     * @return 爬取的数据
+     */
+    public static String webCrawler(String net) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        // 创建URL对象
+        URL url = new URL(net);
+        // 连接网址
+        URLConnection conn = url.openConnection();
+        // 读取数据(字节流-->字符流)
+        InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+        int ch;
+        while ((ch = isr.read()) != -1){
+            sb.append((char) ch);
+        }
+        isr.close();
+        return sb.toString();
+    }
+
+}
